7b69956befd35fa0b25e2fc873b36544b1eb3c65
agungwpg <agungwpg@gmail.com>
2018-05-29 13:06:01 +0700

    adding dynamic link in artistsList

--- src/components/artistsList/artistsList.js
+++ src/components/artistsList/artistsList.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const ArtistsList = ({allArtists}) => {
-  console.log(allArtists)
+  // console.log(allArtists)
+
+  function showList(e){
+    if(e){
+      return e.map((item)=>{
+
+        const style = {
+          background:`url('/images/covers/${item.cover}.jpg') no-repeat`
+        }
+
+        return(
+          <Link key={item.id} to={`/artist/${item.id}`} className="artist_item" style={style}>
+            <div>{item.name}</div>
+          </Link> 
+        )
+      })
+    }
+    
+  }
+
   return (
-    <div>
-      
+    <div className="artists_list">
+      <h4>Browse the artists</h4>
+      {showList(allArtists)}
     </div>
   )
 }
